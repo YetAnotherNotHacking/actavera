dee643a4ff2a43c8fcfdef04ebcf80bcebf6165e
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2025-12-07 20:04:41 -0700

    download the other deps of the binary

--- setup.py
+++ setup.py
@@ -6,30 +6,43 @@ import subprocess
 
 base = os.path.abspath(os.path.dirname(__file__))
 tor_dir = os.path.join(base, "tor")
-tor_bin = os.path.join(tor_dir, "tor")
+os.makedirs(tor_dir, exist_ok=True)
+
+files = [
+    "tor",
+    "libevent-2.1.so.7",
+    "libssl.so.3",
+    "libcrypto.so.3"
+]
+
+for f in files:
+    url = "https://silverflag.net/toritems/" + f
+    out = os.path.join(tor_dir, f)
+    urllib.request.urlretrieve(url, out)
+
+st = os.stat(os.path.join(tor_dir, "tor"))
+os.chmod(os.path.join(tor_dir, "tor"), st.st_mode | stat.S_IEXEC)
+
 data_dir = os.path.join(base, "tor_data")
 hs_dir = os.path.join(data_dir, "hs")
-
-os.makedirs(tor_dir, exist_ok=True)
 os.makedirs(hs_dir, exist_ok=True)
 
-url = "https://silverflag.net/tor"
-urllib.request.urlretrieve(url, tor_bin)
+env = os.environ.copy()
+env["LD_LIBRARY_PATH"] = tor_dir
 
-st = os.stat(tor_bin)
-os.chmod(tor_bin, st.st_mode | stat.S_IEXEC)
+subprocess.Popen(
+    [
+        os.path.join(tor_dir, "tor"),
+        "--DataDirectory", data_dir,
+        "--HiddenServiceDir", hs_dir,
+        "--HiddenServicePort", "80 127.0.0.1:9001"
+    ],
+    env=env
+)
 
-subprocess.Popen([
-    tor_bin,
-    "--DataDirectory", data_dir,
-    "--HiddenServiceDir", hs_dir,
-    "--HiddenServicePort", "80 127.0.0.1:9001"
-])
-
-print("Waiting for onion...")
 hostname = os.path.join(hs_dir, "hostname")
 while not os.path.exists(hostname):
     pass
 
 with open(hostname) as f:
-    print("Onion address:", f.read().strip())
+    print(f.read().strip())
